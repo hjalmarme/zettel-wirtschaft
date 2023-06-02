3030bb5d00af7eeb0e3c880c499a33c425af0a10
Hjalmar Merker <hjalmar.merker@gmail.com>
2023-06-03 00:57:27 +0200

    added an export service

---
+++ Services/ExportService.cs
@@ -0,0 +1,42 @@
+﻿using ClosedXML.Excel;
+using System.Globalization;
+
+namespace ZettelWirtschaft.Services
+{
+    public class ExportService : IExportService
+    {
+        public (byte[] content, string fileType) CreateXlsx(IEnumerable<(Models.Order order, Models.MenuItem item)> orderItems)
+        {
+            // workbook / worksheet
+            using var workbook = new XLWorkbook();
+            var worksheet = workbook.Worksheets.Add("Sheet1");
+
+            // header
+            worksheet.Cell("A1").Value = "ID";
+            worksheet.Cell("B1").Value = "Date";
+            worksheet.Cell("C1").Value = "Name";
+            worksheet.Cell("D1").Value = "Price";
+
+            // content
+            int row = 2;
+            foreach (var item in orderItems)
+            {
+                worksheet.Cell($"A{row}").Value = item.order.Id;
+                worksheet.Cell($"B{row}").Value = item.order.Date.ToString(CultureInfo.CurrentCulture);
+                worksheet.Cell($"C{row}").Value = item.item.Name;
+                worksheet.Cell($"D{row}").Value = item.item.Price;
+                row++;
+            }
+
+            // Convert workbook to a byte array
+            byte[] byteArray;
+            using (var memoryStream = new MemoryStream())
+            {
+                workbook.SaveAs(memoryStream);
+                byteArray = memoryStream.ToArray();
+            }
+
+            return (byteArray, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
+        }
+    }
+}
---
+++ Services/IExportService.cs
@@ -0,0 +1,7 @@
+﻿namespace ZettelWirtschaft.Services
+{
+    public interface IExportService
+    {
+        public (byte[] content, string fileType) CreateXlsx(IEnumerable<(Models.Order order, Models.MenuItem item)> orderItems);
+    }
+}
